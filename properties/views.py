e701dac1659565cec619991ca92ab50a406902b1
Florence Ndabambi <flossiendabambi@gmail.com>
2025-07-11 23:03:31 +0200

    edit views.py

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,8 @@
 from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
 from django.http import JsonResponse
-from django.shortcuts import render
 from .models import Property
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all().values('title', 'description', 'price', 'location', 'created_at')
-    return JsonResponse(list(properties), safe=False)
+    properties = Property.objects.values('title', 'description', 'price', 'location', 'created_at')
+    return JsonResponse({"data": list(properties)}, safe=False)
