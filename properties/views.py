10d139de6b4ef4b5d82355c550dabe6d1eb1fa5b
Florence Ndabambi <flossiendabambi@gmail.com>
2025-07-11 23:06:45 +0200

    low level caching

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,6 @@
-from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .models import Property
+from .utils import get_all_properties
 
-@cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.values('title', 'description', 'price', 'location', 'created_at')
-    return JsonResponse({"data": list(properties)}, safe=False)
+    properties = get_all_properties()
+    return JsonResponse({"data": properties}, safe=False)
